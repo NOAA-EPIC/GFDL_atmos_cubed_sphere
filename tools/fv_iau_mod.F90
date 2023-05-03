c064866966d64345edd8b8b6a56fbbc246206f07
Mark A Potts <Mark.Potts@noaa.gov>
2023-05-03 18:37:22 +0000

    removed bad inc copy

--- tools/fv_iau_mod.F90
+++ tools/fv_iau_mod.F90
@@ -205,7 +205,7 @@ subroutine IAU_initialize (IPD_Control, IAU_Data,Atm,mygrid,Init_parm, testing, 
 
     npz = IPD_Control%levs
     fname = 'INPUT/'//trim(IPD_Control%iau_inc_files(1))
-
+#if 0
     if( file_exist(fname) ) then
       call open_ncfile( fname, ncid )        ! open the file
       call get_ncdim1( ncid, 'lon',   im)
@@ -256,7 +256,7 @@ subroutine IAU_initialize (IPD_Control, IAU_Data,Atm,mygrid,Init_parm, testing, 
         im, jm, lon, lat, id1, id2, jdc, s2c, &
         agrid)
     deallocate ( lon, lat,agrid )
-
+#endif
 !mp probably need to do this
     allocate(IAU_Data%ua_inc(is:ie, js:je, km))
     allocate(IAU_Data%va_inc(is:ie, js:je, km))
@@ -296,12 +296,13 @@ subroutine IAU_initialize (IPD_Control, IAU_Data,Atm,mygrid,Init_parm, testing, 
        iau_state%wt_normfact = (2*nstep+1)/normfact
     endif
     !call read_iau_forcing(IPD_Control,iau_state%inc1,'INPUT/'//trim(IPD_Control%iau_inc_files(1)))
+    write(6,*) 'calling read_netcdf_inc with ',trim(IPD_Control%iau_inc_files(1))
     call read_netcdf_inc('INPUT/'//trim(IPD_Control%iau_inc_files(1)),iau_state%inc1,Atm,mygrid,.false.)
-    iau_state%inc1%ua_inc(is:ie, js:je, :)=iau_inc1%ua_inc(is:ie, js:je, :)
-    iau_state%inc1%va_inc(is:ie, js:je, :)=iau_inc1%va_inc(is:ie, js:je, :)
-    iau_state%inc1%temp_inc(is:ie, js:je, :)=iau_inc1%temp_inc(is:ie, js:je, :)
-    iau_state%inc1%delp_inc(is:ie, js:je, :)=iau_inc1%delp_inc(is:ie, js:je, :)
-    iau_state%inc1%tracer_inc(is:ie, js:je, :, :)=iau_inc1%tracer_inc(is:ie, js:je, :,:)
+!   iau_state%inc1%ua_inc(is:ie, js:je, :)=iau_inc1%ua_inc(is:ie, js:je, :)
+!   iau_state%inc1%va_inc(is:ie, js:je, :)=iau_inc1%va_inc(is:ie, js:je, :)
+!   iau_state%inc1%temp_inc(is:ie, js:je, :)=iau_inc1%temp_inc(is:ie, js:je, :)
+!   iau_state%inc1%delp_inc(is:ie, js:je, :)=iau_inc1%delp_inc(is:ie, js:je, :)
+!   iau_state%inc1%tracer_inc(is:ie, js:je, :, :)=iau_inc1%tracer_inc(is:ie, js:je, :,:)
     if (nfiles.EQ.1) then  ! only need to get incrments once since constant forcing over window
        call setiauforcing(IPD_Control,IAU_Data,iau_state%wt)
     endif
@@ -315,11 +316,11 @@ subroutine IAU_initialize (IPD_Control, IAU_Data,Atm,mygrid,Init_parm, testing, 
        iau_state%hr2=IPD_Control%iaufhrs(2)
 !      call read_iau_forcing(IPD_Control,iau_state%inc2,'INPUT/'//trim(IPD_Control%iau_inc_files(2)))
        call read_netcdf_inc('INPUT/'//trim(IPD_Control%iau_inc_files(2)),iau_state%inc2,Atm,mygrid,.false.)
-       iau_state%inc2%ua_inc(is:ie, js:je, :)=iau_inc2%ua_inc(is:ie, js:je, :)
-       iau_state%inc2%va_inc(is:ie, js:je, :)=iau_inc2%va_inc(is:ie, js:je, :)
-       iau_state%inc2%temp_inc(is:ie, js:je, :)=iau_inc2%temp_inc(is:ie, js:je, :)
-       iau_state%inc2%delp_inc(is:ie, js:je, :)=iau_inc2%delp_inc(is:ie, js:je, :)
-       iau_state%inc2%tracer_inc(is:ie, js:je, :, :)=iau_inc2%tracer_inc(is:ie, js:je, :,:)
+!      iau_state%inc2%ua_inc(is:ie, js:je, :)=iau_inc2%ua_inc(is:ie, js:je, :)
+!      iau_state%inc2%va_inc(is:ie, js:je, :)=iau_inc2%va_inc(is:ie, js:je, :)
+!      iau_state%inc2%temp_inc(is:ie, js:je, :)=iau_inc2%temp_inc(is:ie, js:je, :)
+!      iau_state%inc2%delp_inc(is:ie, js:je, :)=iau_inc2%delp_inc(is:ie, js:je, :)
+!      iau_state%inc2%tracer_inc(is:ie, js:je, :, :)=iau_inc2%tracer_inc(is:ie, js:je, :,:)
     endif
 !   print*,'in IAU init',dt,rdt
     IAU_data%drymassfixer = IPD_control%iau_drymassfixer
